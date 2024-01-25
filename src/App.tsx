ee9c555d1827130915b6ef0ef80a28b12fefaf05
wiulma <giorgio@frescofrigo.it>
2024-01-25 18:09:06 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import viteLogo from "./assets/vite.svg";
+import reactLogo from "./assets/react.svg";
+import sassLogo from "./assets/sass.svg";
+import githubLogo from "./assets/github.svg";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+        <h1>
+          <span> Hello world! </span>
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
